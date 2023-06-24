9a022a819ed7d1b239b43d7812bfcce7840ec91a
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2023-06-23 22:46:31 -0300

    fix in connection to api

--- src/Services/movies.ts
+++ src/Services/movies.ts
@@ -1,7 +1,12 @@
 import axios from 'axios'; 
-import { baseUrlMovie } from '../configApi'; 
+import { configDefault, baseApiUpi } from '../configApi'; 
 
 export function getMovies () {
-    const url = baseUrlMovie("day", "trending"); 
-    return axios.get(url);
+    const url = `${baseApiUpi}/trending/movie/day`;
+
+    const config = {
+        params: configDefault
+    }
+    
+    return axios.get(url, config);
 }
